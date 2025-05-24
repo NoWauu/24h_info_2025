c2e98ca64fdd4b6baec83b905f808030847a1220
Omar ID EL MOUMEN <contact@om4r932.fr>
2025-05-24 05:19:54 +0200

    Final push v2

--- Web/lyonlumieres/app/fete/page.tsx
+++ Web/lyonlumieres/app/fete/page.tsx
@@ -7,7 +7,7 @@ export default function Fete() {
         <h1 className="text-3xl text-center font-bold p-8">Fête des Lumières</h1>
             <div className="flex place-content-center flex-row items-center gap-6 p-7 rounded-2xl">
                 <p className="text-lg w-1/2">
-                Imaginez une ville qui, chaque hiver, se transforme en un théâtre de lumière. Les rues deviennent des toiles, les monuments se parent de couleurs féeriques, et des milliers de visages levés vers le ciel s’illuminent d’émerveillement. Bienvenue à Lyon, au cœur de l'une des plus grandes célébrations lumineuses du monde : la Fête des Lumières.
+                Imaginez une ville qui, chaque hiver, se transforme en un théâtre de lumière. Les rues deviennent des toiles, les monuments se parent de couleurs féeriques, et des milliers de visages levés vers le ciel s’illuminent d’émerveillement. Bienvenue à Lyon, au cœur de l&apos;une des plus grandes célébrations lumineuses du monde : la Fête des Lumières.
                 </p>
                 <Image
                     src="/Lyon.png"
